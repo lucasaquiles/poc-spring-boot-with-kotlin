04442be3a46d743352db3f3a33fa9b8534018d81
Lucas Aquiles <lucasaquiles@gmail.com>
2018-10-27 23:50:42 -0300

    config to swagger

--- src/main/kotlin/organization/HtmlController.kt
+++ src/main/kotlin/organization/HtmlController.kt
@@ -4,14 +4,15 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController
 class HtmlController(private val repository: CompanyRepository){
 
-    @GetMapping("/")
-    fun organization(model:Model):String {
+    @GetMapping("/organization")
+    fun organization(model:Model): Model {
         model["title"] = "Organization"
         model["companies"] = repository.findAllByOrderByAddedAtDesc().map { it.name }
-        return "organization"
+        return model;
     }
 }
